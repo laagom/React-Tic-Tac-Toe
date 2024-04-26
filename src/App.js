8d1766d61ed0c2d0b6a2604e4f6306a61acff061
laagom <cjyooong@gmail.com>
2024-04-26 16:44:47 +0900

    게임판 Board&Square 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function Square() {
-  return <button className="square">X</button>;
+import './css/App.css';
+function Square({ value }) {
+  return <button className="square">{ value }</button>;
 }
 
-export default Square;
+function Board() {
+  return (
+    <>
+      <div>
+        <div className="board-row">
+          <Square value="1"/>
+          <Square value="2"/>
+          <Square value="3"/>
+        </div>
+        <div className="board-row">
+          <Square value="4"/>
+          <Square value="5"/>
+          <Square value="6"/>
+        </div>
+        <div className="board-row">
+          <Square value="7"/>
+          <Square value="8"/>
+          <Square value="9"/>
+        </div>
+      </div>
+    </>
+    );
+}
+
+export default Board;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 
-import Square from './App';
+import Board from './App';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <Square />
+    <Board />
   </StrictMode>
 );
