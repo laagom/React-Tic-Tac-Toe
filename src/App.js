986f047c8214bf5d7906c406aca8383278771ac1
laagom <cjyooong@gmail.com>
2024-04-29 11:46:01 +0900

    과거로 돌아가기

--- src/App.js
+++ src/App.js
@@ -9,28 +9,24 @@ function Square({value, onSquareClick}) {
   );
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay}) {
   function handleClick(i) {
     // 이미 선택된 경우 Stop
-    if(squares[i] || calculateWinner(squares)) {
+    if( calculateWinner(squares) || squares[i]) {
       return;
     }
-
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
 
-    // 다음에 추가될 모형의 구분 값
-    setXIsNext(!xIsNext);
+    // onPlay Alias로 전달받은 Game > handlePlay
+    onPlay(nextSquares);
   }
 
+  // 문구 설정
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -63,6 +59,7 @@ function Board() {
     );
 }
 
+// 승자 확인
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -72,15 +69,56 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
-  for(let i = 0; i < lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
   return null;
 }
 
-export default Board;
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  // History 남기기 & 차례 설정
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length -1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if(move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{ description }</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
